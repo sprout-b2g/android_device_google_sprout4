1672dea8564db5ed9a2e0beadc2cadf94e273ee4
Anik <anik9280@gmail.com>
2015-12-18 20:27:07 +0600

    sprout: Initial commit for FireFox OS
    Signed-off-by: Anik <anik9280@gmail.com>

--- sprout4.mk
+++ sprout4.mk
@@ -23,3 +23,33 @@ $(call inherit-product, device/google/sprout-common/sprout.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/init.sprout.rc:root/init.sprout.rc \
     $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout
+
+# for Gecko to use the correct resolution assets
+# Valid options are: 1.5 | 2 | 2.25
+GAIA_DEV_PIXELS_PER_PX := 1.5
+
+# for Gecko to use the correct boot animation
+# Valid options are: hvga | fwvga | qHD | 720p | 1080p
+BOOTANIMATION_ASSET_SIZE := fwvga
+
++# for Gonk to support Camera blobs
+PRODUCT_PACKAGES += \
+ libandroid
+
+# for Gecko to support usb mass storage
+# You may need to add mass_storage to init.oem.usb.rc
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ persist.sys.usb.config=mass_storage
+
+# for Gecko to support virtual home button
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.moz.has_home_button=0
+
+# for Gecko to support Dual SIM
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.radio.multisim.config=dsds \
+ ro.moz.ril.numclients=2 \
+ ro.moz.ril.subscription_control=true
+
+# for locales
+PRODUCT_LOCALES := en_US
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
-add_lunch_combo cm_sprout4-userdebug
-add_lunch_combo cm_sprout4-eng
+add_lunch_combo full_sprout-userdebug
